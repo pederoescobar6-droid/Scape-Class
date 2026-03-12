e7cb9668258918decf417ee7906f0fd9d7233a50
ivan <155097068+KnightTortillero@users.noreply.github.com>
2026-03-12 13:34:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<juegos>
+  <juego1>
+    <nombre>Juego 1</nombre>
+    <ruta>practicas/Educabaran/juego1.html</ruta>
+    <habilitado>si</habilitado>
+  </juego1>
+  <juego2>
+    <nombre>Juego 2</nombre>
+    <ruta>practicas/Educabaran/juego4.html</ruta>
+    <habilitado>si</habilitado>
+  </juego2>
+  <juego3>
+    <nombre>Juego 3</nombre>
+    <ruta>practicas/Educabaran/juego3.html</ruta>
+    <habilitado>si</habilitado>
+  </juego3>
+  <juego4>
+    <nombre>Juego 4</nombre>
+    <ruta>practicas/Educabaran/juego4.html</ruta>
+    <habilitado>si</habilitado>
+  </juego4>
+</juegos>
