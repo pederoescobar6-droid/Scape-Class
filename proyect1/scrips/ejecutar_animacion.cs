1f6f0a42c08f2d4c68d8f015ed3c4a891d42ed2f
pederoescobar6-droid <pederoescobar6@gmail.com>
2026-03-20 13:21:32 +0100

    Add files via upload

---
+++ proyect1/scrips/ejecutar_animacion.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+public partial class ejecutar_animacion : CharacterBody2D
+{
+	[Export] public float Speed { get; set; } = 50f;
+
+	private AnimatedSprite2D _animatedSprite;
+
+	public override void _Ready()
+	{
+		// 1. Buscamos el nodo por su nombre en la escena
+		_animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		
+		// 2. Ejecutamos la animación por su nombre (el que sale en la lista de abajo)
+		// Casi siempre la primera animación se llama "default"
+		_animatedSprite.Play("idle"); 
+	}
+}
---
+++ proyect1/scrips/fin_juego.cs
@@ -0,0 +1,46 @@
+using Godot;
+
+public partial class fin_juego : Area2D
+{
+// Cambia esto a la ruta real de tu escena:
+// por ejemplo: "res://Escenas/Nivel2.tscn"
+[Export]
+public string NextScenePath = "res://Escenas/node_2d/pasillo_2.tscn";
+
+public override void _Ready()
+{
+BodyEntered += OnBodyEntered;
+Monitoring = true;
+}
+
+private void OnBodyEntered(Node2D body)
+{
+GD.Print("=== DEBUG ===");
+GD.Print("body.Name = '" + body.Name + "'");
+GD.Print("body.GetType() = " + body.GetType());
+
+if (body.Name.ToString() == "Protagonista" ||
+body.Name.ToString().Contains("Protagonista") ||
+body is Protagonista)
+{
+GD.Print(" LÁPIZ RECOGIDO!");
+
+// Cambiar de escena
+if (!string.IsNullOrEmpty(NextScenePath))
+{
+GetTree().ChangeSceneToFile(NextScenePath); // Godot 4 C#[web:5][web:8]
+}
+else
+{
+GD.PrintErr("NextScenePath no está configurado");
+
+}
+
+QueueFree();
+}
+else
+{
+GD.Print(" No es Protagonista");
+}
+}
+}
